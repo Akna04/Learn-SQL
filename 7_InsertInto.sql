bb717bbec3abbb1685ae8e5de295a25505f2f47b
Rahayus12 <102126439+Rahayus12@users.noreply.github.com>
2022-06-03 16:23:06 +0700

    insert into

---
+++ 7_InsertInto.sql
@@ -0,0 +1,4 @@
+The insert into statement is used to insert new ercords in a table
+
+INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES ('Freed', 'Hardy B.', 'Skagen 21', 'Stavanger', '4006', 'Italy');
